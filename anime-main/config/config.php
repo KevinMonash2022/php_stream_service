c5bb99ba10e1ab975cbe8cbcd3e565b64a9c9e9e
KAIXIN ZHANG <145351389+KevinMonash2022@users.noreply.github.com>
2024-02-14 11:21:04 +1100

    Created config file

--- anime-main/config/config.php
+++ anime-main/config/config.php
@@ -1,5 +1,7 @@
 <?php
 
+try {
+
     //host
     define("HOST", "localhost");
 
@@ -12,10 +14,15 @@
     //pass
     define("PASS", "");
 
-    $conn = new PDO("mysql:host=".HOST.";dbname=".anime."", USER, PASS);
+    $conn = new PDO("mysql:host=".HOST.";dbname=".DBNAME."", USER, PASS);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    if($conn == true) {
-        echo "db connection is successful";
-    } else {
-        echo "db error"
-    }
+    // if($conn == true) {
+    //     echo "db connection is successful";
+    // } else {
+    //     echo "db error";
+    // }
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+?>
